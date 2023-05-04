35b0a356a88c85cd256af0daef0e79285f822863
Viralwow <132092012+Viralwow@users.noreply.github.com>
2023-05-04 06:25:34 +0500

    Create 404.js

---
+++ 404.js
@@ -0,0 +1,19 @@
+{
+
+    "cleanUrls": true,
+
+    "redirects": [
+
+        {
+
+            "source": "/(.*)",
+
+            "destination": "/status/404/index.html",
+
+            "statusCode": 404
+
+        }
+
+    ]
+
+}
